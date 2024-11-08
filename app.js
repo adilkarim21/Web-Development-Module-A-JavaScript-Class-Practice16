5c61391e91c0aa47e3aa3930881d7e0c21f16a42
Adil Khosa <muhammadadilmemon79@gmail.com>
2024-11-08 15:11:36 +0500

    HTML & JavaScript

---
+++ app.js
@@ -0,0 +1,49 @@
+// var userObj = {
+//     name: 'adil',
+//     email: 'adil123@gmail.com',
+//     password: '12341234',
+//     status: true,
+// };
+
+// console.log(JSON.stringify(userObj));
+
+// localStorage.setItem('BioData',JSON.stringify(userObj));
+
+// localStorage.setItem('username','adil');
+// localStorage.setItem('city','karachi');
+// localStorage.setItem('age',17);
+
+
+// var response1 = localStorage.getItem('BioData');
+
+// console.log(JSON.parse(response1));
+
+// var response2 = localStorage.getItem('city');
+
+// console.log(response2);
+
+// localStorage.removeItem('age');
+
+// localStorage.removeItem('city');
+
+// localStorage.clear();
+
+
+
+
+// var userInfo = {
+//     Username: 'adil',
+//     Useremail: 'adil123@gmail.com',
+//     Userpassword: '12341234',
+// };
+
+// function sendData() {
+//     localStorage.setItem('UserInput',JSON.stringify(userInfo));
+// }
+
+// function getData() {
+//     var response = localStorage.getItem('UserInput');
+//     console.log(JSON.parse(response));
+//     localStorage.clear();
+// }
+
